78489f0b114c1dd5f42fe9bcd35d9e311254f6d3
anton.makarov <antowkabond1@gmail.com>
2025-10-09 18:11:53 +0100

    enhance modules by integrating PrismaModule and updating service lifecycle management

--- src/infrastructure/database/prisma/prisma.service.ts
+++ src/infrastructure/database/prisma/prisma.service.ts
@@ -3,14 +3,20 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
+    process.on('beforeExit', async () => {
+      await this.$disconnect();
       await app.close();
     });
   }
 }
-
